38a06abf439ff9e3e940c835aa9208a876f3c85a
andrespv12 <c4rlos4ndres@gmail.com>
2024-09-08 18:23:02 -0500

    creacion de componentes

---
+++ frontend/src/app/acse/acse.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-acse',
+  standalone: true,
+  imports: [],
+  templateUrl: './acse.component.html',
+  styleUrl: './acse.component.css'
+})
+export class AcseComponent {
+
+}
